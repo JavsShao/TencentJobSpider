41015c3efb1456845d8b349ff675159cd690e1b9
hamdi <javs_shao@163.com>
2018-12-25 16:41:12 +0800

    配置文件_开启ITEM_PIPELINES

--- Tencent/Tencent/settings.py
+++ Tencent/Tencent/settings.py
@@ -64,9 +64,10 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'Tencent.pipelines.TencentPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   # 'Tencent.pipelines.TencentPipeline': 300,
+    'Tencent.Pipelines.TencentPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
