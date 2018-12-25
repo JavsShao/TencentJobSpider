7f13cda08b2660d6891332a21ab98324a9f74804
hamdi <javs_shao@163.com>
2018-12-25 16:06:38 +0800

    编写items_创建字段

--- Tencent/Tencent/items.py
+++ Tencent/Tencent/items.py
@@ -11,4 +11,9 @@ import scrapy
 class TencentItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    position_info = scrapy.Field()
+    people_number = scrapy.Field()
+    work_location = scrapy.Field()
+    publish_time = scrapy.Field()
+
